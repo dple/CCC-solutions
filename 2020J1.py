e12d956bf8a30bd8398dfb94aaa75cd926a242d1
Phong Le <le.duc.phong@gmail.com>
2023-02-13 10:37:53 -0500

    Initial commit

---
+++ 2020J1.py
@@ -0,0 +1,9 @@
+small=int(input())
+medium=int(input())
+big=int(input())
+
+happiness=small+2*medium+3*big
+if happiness >= 10:
+    print("happy")
+else:
+    print("sad")
---
+++ 2020J2.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    P=int(input())
+    N=int(input())
+    R=int(input())
