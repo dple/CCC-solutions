fd64d812cf414505c864a70d57e272cf01e73106
Phong Le <le.duc.phong@gmail.com>
2023-02-13 11:33:07 -0500

    New solutions

--- 2020J1.py
+++
@@ -1,9 +0,0 @@
-small=int(input())
-medium=int(input())
-big=int(input())
-
-happiness=small+2*medium+3*big
-if happiness >= 10:
-    print("happy")
-else:
-    print("sad")
--- 2020J2.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    P=int(input())
-    N=int(input())
-    R=int(input())
